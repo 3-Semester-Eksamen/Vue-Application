6a67f1e0c3ece47ed87e67d6e42986583f9c1e9a
MitzDK <victorwilliamjejlskov@gmail.com>
2022-12-07 13:42:43 +0100

    Update SensorsPage.vue

--- src/components/SensorsPage.vue
+++ src/components/SensorsPage.vue
@@ -1,37 +1,42 @@
 <template>
-    <div class="w-8/12 mx-auto">
+  <div class="w-8/12 mx-auto">
     <div class="flex flex-col">
       <div class="overflow-x-auto sm:rounded-lg">
         <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden">
-            <table class="min-w-full divide-y divide-gray-700 table-fixed">
-              <thead class="bg-gray-700">
-                <tr>
+            <table
+              class="min-w-full divide-y divide-gray-700 table-fixed grid grid-cols-5">
+              <thead class="bg-gray-700 col-span-5">
+                <tr class="grid grid-cols-5">
                   <th
-                    scope="col"
-                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white">
-                    Assigned Name
+                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white col-span-1">
+                    Unassigned Name
                   </th>
+                  <th></th>
+                  <th></th>
                   <th
-                    scope="col"
-                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white">
-                    Assigned MAC-Address
+                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white col-span-1">
+                    MAC-Address
                   </th>
+                  <th></th>
                 </tr>
               </thead>
-              <tbody class="bg-gray-800 divide-y divide-gray-700">
+              <tbody class="bg-gray-800 divide-y divide-gray-700 col-span-5">
                 <tr
-                  class="hover:bg-gray-700"
+                  class="hover:bg-gray-700 grid grid-cols-5"
                   v-for="sensor in sensorArray"
                   v-bind:key="sensor.macAddress">
                   <td
-                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">
+                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap col-span-1">
                     {{ sensor.name }}
                   </td>
+                  <td></td>
+                  <td></td>
                   <td
-                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">
+                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap col-span-1">
                     {{ sensor.macAddress }}
                   </td>
+                  <td></td>
                 </tr>
               </tbody>
             </table>
@@ -43,34 +48,35 @@
       <div class="overflow-x-auto sm:rounded-lg">
         <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden">
-            <table class="min-w-full divide-y divide-gray-700 table-fixed">
-              <thead class="bg-gray-700">
-                <tr>
+            <table
+              class="min-w-full divide-y divide-gray-700 table-fixed grid grid-cols-5">
+              <thead class="bg-gray-700 col-span-5">
+                <tr class="grid grid-cols-5">
                   <th
-                    scope="col"
-                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white">
+                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white col-span-2">
                     Unassigned Name
                   </th>
                   <th
-                    scope="col"
-                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white">
+                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white col-span-2">
                     MAC-Address
                   </th>
+                  <th></th>
                 </tr>
               </thead>
-              <tbody class="bg-gray-800 divide-y divide-gray-700">
+              <tbody class="bg-gray-800 divide-y divide-gray-700 col-span-5">
                 <tr
-                  class="hover:bg-gray-700"
+                  class="hover:bg-gray-700 grid grid-cols-5"
                   v-for="sensor in unassignedArray"
                   v-bind:key="sensor.macAddress">
                   <td
-                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">
+                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap col-span-2">
                     {{ sensor.name }}
                   </td>
                   <td
-                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap">
+                    class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap col-span-2">
                     {{ sensor.macAddress }}
                   </td>
+                  <td></td>
                 </tr>
               </tbody>
             </table>
@@ -81,44 +87,44 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue"
 import type { sensorType } from "../types/sensorType"
-import type { PropType } from 'vue'
-import axios from 'axios'
+import type { PropType } from "vue"
+import axios from "axios"
 
 export default defineComponent({
-    data() {
-        return {
-            sensorArray: this.sensors,
-            unassignedArray: this.unassignedSensors
-        }
-    },
-    props: {
-        sensors: {
-            required: true,
-            type: Object as PropType<sensorType[]>
-        },
-        unassignedSensors: {
-          required: true,
-          type: Object as PropType<sensorType[]>
-        }
-    },
-    methods: {
-        async getSensors() {
-            let assignedUrl = "https://localhost:7220/api/Sensors"
-            let assignedResponse = await axios.get(assignedUrl)
-            this.sensorArray = assignedResponse.data
-            let unassignedUrl = "https://localhost:7220/api/Sensors?unassigned=true"
-            let unassginedResponse = await axios.get(unassignedUrl)
-            this.unassignedArray = unassginedResponse.data
-            this.$emit("updateSensors", this.sensorArray, this.unassignedArray)
-        }
-    },
-    mounted() {
-        if(!this.sensorArray.length) {
-            this.getSensors()
-            console.log("Getting Sensors")
-        }
+  data() {
+    return {
+      sensorArray: this.sensors,
+      unassignedArray: this.unassignedSensors,
     }
+  },
+  props: {
+    sensors: {
+      required: true,
+      type: Object as PropType<sensorType[]>,
+    },
+    unassignedSensors: {
+      required: true,
+      type: Object as PropType<sensorType[]>,
+    },
+  },
+  methods: {
+    async getSensors() {
+      let assignedUrl = "https://localhost:7220/api/Sensors"
+      let assignedResponse = await axios.get(assignedUrl)
+      this.sensorArray = assignedResponse.data
+      let unassignedUrl = "https://localhost:7220/api/Sensors?unassigned=true"
+      let unassginedResponse = await axios.get(unassignedUrl)
+      this.unassignedArray = unassginedResponse.data
+      this.$emit("updateSensors", this.sensorArray, this.unassignedArray)
+    },
+  },
+  mounted() {
+    if (!this.sensorArray.length) {
+      this.getSensors()
+      console.log("Getting Sensors")
+    }
+  },
 })
 </script>
