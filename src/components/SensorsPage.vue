45737c5597364cddf40c1f941f6077851e78e624
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2022-12-07 15:45:53 +0100

    Sensor Filter

--- src/components/SensorsPage.vue
+++ src/components/SensorsPage.vue
@@ -4,13 +4,23 @@
       <div class="overflow-x-auto sm:rounded-lg">
         <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden">
+            <div class="my-4 grid grid-cols-12">
+              <div class="col-span-1"></div>
+              <div class="col-span-3 inline-flex">
+                <input type="text" placeholder="Name or MacAddress" v-model="filterValue">
+                <button class="bg-green-700 text-gray-300 hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium py-2"
+                @click="filterByInput(filterValue)">Filter</button>
+              </div>
+              <div class="col-span-6"></div>
+              <div>11</div>
+            </div>
             <table
               class="min-w-full divide-y divide-gray-700 table-fixed grid grid-cols-5">
               <thead class="bg-gray-700 col-span-5">
                 <tr class="grid grid-cols-5">
                   <th
                     class="py-3 px-6 text-xs font-medium tracking-wider text-left text-white col-span-1">
-                    Unassigned Name
+                    Assigned Name
                   </th>
                   <th></th>
                   <th></th>
@@ -36,7 +46,16 @@
                     class="py-4 px-6 text-sm font-medium text-white whitespace-nowrap col-span-1">
                     {{ sensor.macAddress }}
                   </td>
-                  <td></td>
+                  <td class="py-2 px-6 text-sm font-medium text-white whitespace-nowrap col-span-1">
+                    <button
+                    class="bg-green-700 text-gray-300 hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium py-2"
+                    @click="deleteSensor"
+                    >Rediger</button>
+                    <button 
+                    class="bg-red-700 text-gray-300 hover:bg-gray-700 hover:text-white px-3 rounded-md text-sm font-medium py-2"
+                    @click="editSensor(sensor)"
+                    >Delete</button>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -97,6 +116,7 @@ export default defineComponent({
     return {
       sensorArray: this.sensors,
       unassignedArray: this.unassignedSensors,
+      filterValue: ''
     }
   },
   props: {
@@ -118,6 +138,19 @@ export default defineComponent({
       let unassginedResponse = await axios.get(unassignedUrl)
       this.unassignedArray = unassginedResponse.data
       this.$emit("updateSensors", this.sensorArray, this.unassignedArray)
+    },
+    async deleteSensor() {
+    },
+    async editSensor(sensor: sensorType) {
+    },
+    async filterByInput(input: string){
+      await this.getSensors()
+      const tempArray: any = this.sensorArray.filter(s => {
+        if (s.name.toLowerCase().includes(input.toLowerCase())) return true
+        else if (s.macAddress.toLowerCase().includes(input.toLowerCase())) return true
+        else return false
+      })
+      if (tempArray) this.sensorArray = tempArray
     },
   },
   mounted() {
